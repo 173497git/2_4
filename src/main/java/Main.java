ea8b0c058719465948a30e36f1defe6807f93e98
173497git <161622376+173497git@users.noreply.github.com>
2024-03-13 16:29:46 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("Suma wszystkich liczb nieparzystych od 0 do 100 wynosi: " + sum);
+    }
+}
